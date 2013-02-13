d0610063278b48e7559b3471fd9ec88e5fb0374f
Fitz Ryland <fitzryland@gmail.com>
2013-02-13 09:52:06 -0800

    deleted calendar from events page

--- wp-content/themes/caTesting/events.php
+++ wp-content/themes/caTesting/events.php
@@ -67,11 +67,11 @@ Template Name: Events
 			<p><?php echo $fine; ?></p>
 		</div>
 	</div>
-	<div class="cal">
-		<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-			<?php the_content(); ?>
-		<?php endwhile; ?>
-	</div>
+	<!--<div class="cal">-->
+		<?php// if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+			<?php// the_content(); ?>
+		<?php// endwhile; ?>
+	<!--</div>-->
 	<div class="sidebar">
 	</div>
 </section>
